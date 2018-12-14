107ee8eeaedff218474ad8643c277f6567aae084
HuYong <huy@hadlinks.com>
2018-12-14 10:09:46 +0800

    AddClassA

---
+++ GitTester/GitTester/ClassA.h
@@ -0,0 +1,17 @@
+//
+//  ClassA.h
+//  GitTester
+//
+//  Created by HuYong on 2018/12/14.
+//  Copyright © 2018 Ningbo Hadlinks IOT Science&Technology Co.,Ltd. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ClassA : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ GitTester/GitTester/ClassA.m
@@ -0,0 +1,13 @@
+//
+//  ClassA.m
+//  GitTester
+//
+//  Created by HuYong on 2018/12/14.
+//  Copyright © 2018 Ningbo Hadlinks IOT Science&Technology Co.,Ltd. All rights reserved.
+//
+
+#import "ClassA.h"
+
+@implementation ClassA
+
+@end
