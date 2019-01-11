67e794d4445945b074fac23dd1e56d2a5417c560
HuYong <huy@hadlinks.com>
2019-01-11 17:02:59 +0800

    add Object1

---
+++ GitTester/GitTester/Object1.h
@@ -0,0 +1,17 @@
+//
+//  Object1.h
+//  GitTester
+//
+//  Created by HuYong on 2019/1/11.
+//  Copyright © 2019 Ningbo Hadlinks IOT Science&Technology Co.,Ltd. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Object1 : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ GitTester/GitTester/Object1.m
@@ -0,0 +1,17 @@
+//
+//  Object1.m
+//  GitTester
+//
+//  Created by HuYong on 2019/1/11.
+//  Copyright © 2019 Ningbo Hadlinks IOT Science&Technology Co.,Ltd. All rights reserved.
+//
+
+#import "Object1.h"
+
+@implementation Object1
+
+- (void)issue1{
+    
+}
+
+@end
